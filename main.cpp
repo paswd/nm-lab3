d1b4262af553227c9978486322f9e609911d7815
paswd <paswd@yandex.ru>
2018-06-08 02:11:41 +0300

    Added method 2

--- main.cpp
+++ main.cpp
@@ -75,6 +75,81 @@ void InterpolationPoly(void) {
 CUBE SPLINE
 ===========
 */
+vector <TNum> CubeSplineSub(size_t size, vector <TNum> in_a, vector <TNum> in_b, vector <TNum> in_c,
+	vector <TNum> in_f) {
+	vector <TNum> res(size);
+	for (size_t i = 1; i < size; i++) {
+		in_c[i] -= in_a[i] * in_b[i - 1] / in_c[i - 1];
+		in_f[i] -= in_a[i] * in_f[i - 1] / in_c[i - 1];
+
+	}
+	res[size - 1] = in_f[size - 1] / in_c[size - 1];
+	for (size_t in = size - 1; in > 0; in--) {
+		size_t i = in - 1;
+		res[i] = (in_f[i] - in_b[i] * res[i + 1]) / in_c[i];
+	}
+	return res;
+}
+
+void CubeSpline(void) {
+	TNum x = 2.66666667;
+	//TNum x = .1;
+	vector <pair <TNum, TNum>> table(0);
+	table.push_back(make_pair(1.0, 2.4142));
+	table.push_back(make_pair(1.9, 1.0818));
+	table.push_back(make_pair(2.8, .50953));
+	table.push_back(make_pair(3.7, .11836));
+	table.push_back(make_pair(4.6, -.24008));
+	/*table.push_back(make_pair(-0.4, 1.9823));
+	table.push_back(make_pair(-0.1, 1.6710));
+	table.push_back(make_pair(0.2, 1.3694));
+	table.push_back(make_pair(0.5, 1.0472));
+	table.push_back(make_pair(0.8, 0.6435));*/
+
+	vector <TNum> a(table.size());
+	vector <TNum> b(table.size());
+	vector <TNum> c(table.size()); 
+	vector <TNum> d(table.size());
+	vector <TNum> h(table.size() - 1);
+
+	for(size_t i = 0; i < table.size(); i++) {
+		a[i] = table[i].second;
+	}
+	for(size_t i = 0; i < table.size() - 1; i++) {
+		h[i] = table[i + 1].first - table[i].first;
+	}
+
+	vector <TNum> sub_a(table.size() - 2);
+	vector <TNum> sub_b(table.size() - 2); 
+	vector <TNum> sub_c(table.size() - 2);
+	vector <TNum> sub_f(table.size() - 2);
+	for(size_t i = 0; i < table.size() - 3; i++) {
+		sub_b[i] = h[i];
+		sub_a[i + 1] = h[i];
+	}
+	for(size_t i = 0; i < table.size() - 2; i++) {
+		sub_c[i] = 2 * (h[i + 1] + h[i]);
+	}
+	for(size_t i = 1; i < table.size() - 1; i++) {
+		sub_f[i - 1] = 3 * ((a[i + 1] - a[i]) / h[i] - (a[i] - a[i - 1]) / h[i - 1]);
+	}
+	c = CubeSplineSub(table.size() - 2, sub_a, sub_b, sub_c, sub_f);
+	//c.insert(c.begin(), 0);
+	c[0] = 0;
+	for(size_t i = 0; i < h.size() - 1; i++) {
+		d[i] = (c[i+1] - c[i]) / (3 * h[i]);
+		b[i] = (a[i + 1] - a[i]) / h[i] - h[i] * (c[i + 1] + 2 * c[i]) / 3;
+	}
+	d[h.size()] = -c.back() / (3 * h.back());
+	b[h.size()] = (a.back() - a[a.size() - 2]) / h.back() + 2 * h.back() * c.back() / 3;
+	size_t i = d.size() - 1;
+	while (table[i].first > x)
+		i--;
+	x -= table[i].first;
+	TNum res = a[i] + x * (b[i] + x * (c[i] + x * d[i]));
+
+	cout << res << endl;
+}
 
 /*
 ========================
@@ -123,7 +198,7 @@ int main(void) {
 			InterpolationPoly();
 			break;
 		case 2:
-			//
+			CubeSpline();
 			break;
 		case 3:
 			//
